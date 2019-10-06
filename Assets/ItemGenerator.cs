2966bf157dd983099ed6494e5c3a48f17c37a0a3
森山ナティー <57.moriyama.natee@gmail.com>
2019-10-06 22:28:14 +0900

    design-AppleGenerator

---
+++ Assets/ItemGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour
+{
+    public GameObject applePrefab;
+    public GameObject bombPrefab;
+    float span = 1.0f;
+    float delta = 0;
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+        if(this.delta > this.span)
+        {
+            this.delta = 0;
+            Instantiate(applePrefab);
+        }
+        
+    }
+}
