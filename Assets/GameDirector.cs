7c86daa75f84c22bf314182ecf19fa4bf489233b
森山ナティー <57.moriyama.natee@gmail.com>
2019-10-11 16:31:54 +0900

    design-GameDirector-Time&Point

--- Assets/GameDirector.cs
+++ Assets/GameDirector.cs
@@ -6,11 +6,24 @@ using UnityEngine.UI;  // UIを使う時は忘れないように注意！！
 public class GameDirector : MonoBehaviour
 {
     GameObject timerText;
+    GameObject pointText;
     float time = 60.0f;
+    int point = 0;
+
+    public void GetApple()
+    {
+        this.point += 100;
+    }
+
+    public void GetBomb()
+    {
+        this.point /= 2;
+    }
     
     void Start()
     {
         this.timerText = GameObject.Find("Time");
+        this.pointText = GameObject.Find("Point");
     }
 
     
@@ -19,5 +32,7 @@ public class GameDirector : MonoBehaviour
         this.time -= Time.deltaTime;
         this.timerText.GetComponent<Text>().text =
             this.time.ToString("F1");
+        this.pointText.GetComponent<Text>().text =
+            this.point.ToString() + " point";
     }
 }
