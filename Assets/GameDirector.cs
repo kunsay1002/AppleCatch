a028949680c3189870e38bdc43f9d3d467e16c9e
森山ナティー <57.moriyama.natee@gmail.com>
2019-10-12 00:31:30 +0900

    design-難易度調整-1

--- Assets/GameDirector.cs
+++ Assets/GameDirector.cs
@@ -9,6 +9,7 @@ public class GameDirector : MonoBehaviour
     GameObject pointText;
     float time = 30.0f;
     int point = 0;
+    GameObject generator;
 
     public void GetApple()
     {
@@ -22,6 +23,7 @@ public class GameDirector : MonoBehaviour
     
     void Start()
     {
+        this.generator = GameObject.Find("ItemGenerator");
         this.timerText = GameObject.Find("Time");
         this.pointText = GameObject.Find("Point");
     }
@@ -30,6 +32,33 @@ public class GameDirector : MonoBehaviour
     void Update()
     {
         this.time -= Time.deltaTime;
+
+        if(this.time < 0)
+        {
+            this.time = 0;
+            this.generator.GetComponent<ItemGenerator>().SetParameter(
+                10000.0f, 0, 0);
+        }
+        else if(0 <= this.time && this.time < 5)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(
+                0.9f, -0.04f, 3);
+        }
+        else if(5 <= this.time && this.time < 10)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(
+                0.4f, -0.06f, 6);
+        }
+        else if(10 <= this.time && this.time < 20)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(
+                0.7f, -0.04f, 4);
+        }
+        else if(20 <= this.time && this.time < 30)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(
+                1.0f, -0.03f, 2);
+        }
         this.timerText.GetComponent<Text>().text =
             this.time.ToString("F1");
         this.pointText.GetComponent<Text>().text =
