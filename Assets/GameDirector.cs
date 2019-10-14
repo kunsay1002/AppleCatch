b23919e27d6bd17a3c0d4f92df4d67f23a2fa592
森山ナティー <57.moriyama.natee@gmail.com>
2019-10-14 17:23:41 +0900

    design-GemeDirector-難易度調整-2

--- Assets/GameDirector.cs
+++ Assets/GameDirector.cs
@@ -42,17 +42,17 @@ public class GameDirector : MonoBehaviour
         else if(0 <= this.time && this.time < 5)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(
-                0.9f, -0.04f, 3);
+                0.7f, -0.04f, 3);
         }
         else if(5 <= this.time && this.time < 10)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(
-                0.4f, -0.06f, 6);
+                0.8f, -0.05f, 6);
         }
         else if(10 <= this.time && this.time < 20)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(
-                0.7f, -0.04f, 4);
+                0.8f, -0.04f, 4);
         }
         else if(20 <= this.time && this.time < 30)
         {
